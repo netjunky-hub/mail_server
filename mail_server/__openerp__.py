cce190124fc62467a88844d2c3a357bf0bafde3e
igor <igor@netjunky.net>
2015-04-30 14:16:24 +0200

    Description change

--- mail_server/__openerp__.py
+++ mail_server/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "Mailing Server",
-    'version': '1.1',
+    'name': "Mail Server",
+    'version': '1.0',
     'category': 'Mail',
     'image': 'logo.png',
     'summary': 'Mass mailing and personalized mail server',
-    'description': """Mailing server""",
+    'description': """Mail server""",
     'author': 'Igor Sjeverac',
     'license': 'AGPL-3',
     'website': "www.netjunky.net",
